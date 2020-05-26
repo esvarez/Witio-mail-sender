44854e33187771a6e6cb13bd9b76c53120ddf28e
Erick Suarez <ericksuarezdev@gmail.com>
2020-05-26 16:12:46 -0500

    Initial commit

---
+++ src/main/java/dev/ericksuarez/mail/sender/service/MailSenderServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.mail.sender.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSenderServiceApplication.class, args);
+	}
+
+}
