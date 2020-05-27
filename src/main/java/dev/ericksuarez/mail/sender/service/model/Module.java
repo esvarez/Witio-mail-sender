5b71489f5d4d72ced5adcff76a832f5c59ffb299
Erick Suarez <ericksuarezdev@gmail.com>
2020-05-26 21:37:55 -0500

    Set entities

---
+++ src/main/java/dev/ericksuarez/mail/sender/service/model/Module.java
@@ -0,0 +1,26 @@
+package dev.ericksuarez.mail.sender.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "modules")
+public class Module {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
