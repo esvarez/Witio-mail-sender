57f33cd57396434d1367f576dc714c2b1bce6e6f
Erick Suarez <ericksuarez@mail.com>
2020-05-29 17:58:19 -0500

    add sentgrid

--- src/main/java/dev/ericksuarez/mail/sender/service/MailSenderServiceApplication.java
+++ src/main/java/dev/ericksuarez/mail/sender/service/MailSenderServiceApplication.java
@@ -2,30 +2,11 @@ package dev.ericksuarez.mail.sender.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.util.Collections;
-import java.util.Set;
 
 @SpringBootApplication
 public class MailSenderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MailSenderServiceApplication.class, args);
-
-		justA(Collections.singleton(new A()));
-		justA(Collections.singleton(new B()));
-	}
-
-	static void justA(Set<A> ok) {
-
-	}
-
-	static class A {
-
-	}
-
-	static class B extends A {
-
 	}
 }
---
+++ src/main/java/dev/ericksuarez/mail/sender/service/service/SendGridService.java
@@ -0,0 +1,30 @@
+package dev.ericksuarez.mail.sender.service.service;
+
+public class SendGridService {
+
+    public void sendMail() {
+/*
+        Email from = new Email("test@example.com");
+        String subject = "Sending with SendGrid is Fun";
+        Email to = new Email("test@example.com");
+        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
+
+ */
+
+    }
+}
